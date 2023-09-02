03a7f66a42adce67464e45fa19773c334b84c278
张奕哲 <51223192+zyz0323@users.noreply.github.com>
2023-09-03 00:06:34 +0800

    Update Cloudflare-Workers-Download-Proxy.js

--- Cloudflare-Workers-Download-Proxy.js
+++ Cloudflare-Workers-Download-Proxy.js
@@ -17,6 +17,14 @@ async function handlePageRequest(request) {
       <head>
         <meta charset="UTF-8">
         <title>文件下载</title>
+        <script>
+          function getDownloadLink() {
+            var urlInput = document.getElementById('url');
+            var downloadLink = document.getElementById('download-link');
+            var proxyLink = window.location.origin + '/download?url=' + encodeURI(urlInput.value);
+            downloadLink.textContent = proxyLink;
+          }
+        </script>
       </head>
       <body>
         <h1>文件下载</h1>
@@ -25,6 +33,11 @@ async function handlePageRequest(request) {
           <input type="text" id="url" name="url" required>
           <button type="submit">下载</button>
         </form>
+        <br>
+        <button onclick="getDownloadLink()">获取下载链接</button>
+        <p id="download-link"></p>
+        <p>作者博客：<a href="https://blog.968968.xyz" target="_blank">https://blog.968968.xyz</a></p>
+        <p>本项目仓库：<a href="https://github.com/zyz0323/Cloudflare-Workers-Download-Proxy" target="_blank">https://github.com/zyz0323/Cloudflare-Workers-Download-Proxy</a></p>
       </body>
     </html>
   `
@@ -44,11 +57,17 @@ async function handleDownloadRequest(request) {
   
   const response = await fetch(downloadUrl)
   const modifiedHeaders = new Headers(response.headers)
-  modifiedHeaders.set('Content-Disposition', 'inline')
+  modifiedHeaders.set('Content-Disposition', `attachment; filename=${getFileName(downloadUrl)}`)
   
   return new Response(response.body, {
     status: response.status,
     statusText: response.statusText,
     headers: modifiedHeaders
   })
+}
+
+function getFileName(url) {
+  const urlObj = new URL(url)
+  const pathname = urlObj.pathname
+  return pathname.substring(pathname.lastIndexOf('/') + 1)
 }
